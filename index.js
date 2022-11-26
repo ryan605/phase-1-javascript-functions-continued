bf4d6348ff63ebfb598aea01433eeed99fa317ce
ryan605 <langatryan26@gmail.com>
2022-11-27 00:13:26 +0300

    assignment done couldnt pass last 2 tests

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function saturdayFun(activity)
+{
+    if(activity==undefined){
+        return `This Saturday, I want to roller-skate!`
+    }else
+      return `This Saturday, I want to ${activity}!`
+}
+const mondayWork= function(task=`go to the office`)
+{
+    return `This Monday, I will ${task}.`
+}
+function wrapAdjective(ussop)
+{   
+    
+}
